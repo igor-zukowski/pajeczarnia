d8cec223218bf39b484d8eb05d6b59451ad41895
igor-zukowski <45369629+igor-zukowski@users.noreply.github.com>
2019-05-03 22:59:42 +0200

    Add files via upload

--- att2/getData.js
+++ att2/getData.js
@@ -10,7 +10,7 @@ window.fbAsyncInit = function() {
 let xhr = new XMLHttpRequest();
 let xhrFb = new XMLHttpRequest();
 let url = "https://script.googleusercontent.com/macros/echo?user_content_key=OZolaW75w2etM1DWiHtCJbyz6daH8UiuiDZMvsARKJa0fTxbq-edk3n69EXZDCySWwiNsvuufqoQYZrXm6NYO9DQc0JY78Uvm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnJzGU1Fnd1jadqsLsgdNthl5omCvg3us9Pync5qAzyHl6n73ml4GpN5mfszHmEqfibiiahthl8jg&lib=MPysWlIpktTqDE8rrb0Fcmz8oNvKE2J6l";
-let urlFb = "https://graph.facebook.com/v3.2/taniszpil/published_posts?access_token=EAAfGFjadkk8BAHjeAEyPhxALQCIH5xZAIkX9kWTexvZBf8Q2Bc3J7Y9OkPNvT7mBgkxkXHRdZChXaTMOauZCf1UA8zWjsuZCWuu36r1iba7fRri3iSUsmBkBK47skoVjit9ua4tpmTCt0dm1HbrLftsjs39pdNwbybnS8OqzZAzgZDZD"
+let urlFb = "https://graph.facebook.com/v3.3/2253117284912079/published_posts?access_token=EAAfGFjadkk8BALTTtwBAnc8u57nNgGQ48GjHvZBnagEgBJ80xg7RhxrhTjVBMDIaEXahFSFfIgq7XztwpsyeMinOiMMtOqdvdMuZB4p73Wz2A1zD5LVdKo8mlq48GKFu0WJzMTytCRJQ4X3amEN73nAmKdwS160ZBNdRiHYSujvaXEDPLZB2"
 let json;
 let jLength;
 let date;
@@ -20,8 +20,8 @@ xhr.open("GET", url, true);
 xhr.addEventListener('load', function() {
     if (this.status === 200) {
         json = JSON.parse(this.responseText);
-        jLength = Object.keys(json.zgloszenia).length
-        console.log(json);
+        jLength = Object.keys(json.zgloszenia).length 
+		console.log(json);
         dataPrint();
     }
 })
@@ -66,10 +66,11 @@ function dataPrint(){
 
     let box = document.getElementById('zgloszeniData');
     //section.className = "smallMargin2";
+  
     box.innerHTML = box.innerHTML + '<p id="contentP"> Imie: '+json.zgloszenia[k].Imie; '</p>'
     box.innerHTML = box.innerHTML + '<p id="contentP"> Nazwisko: '+json.zgloszenia[k].Nazwisko; '</p>'
     box.innerHTML = box.innerHTML + '<p id="contentP"> Kurs: '+json.zgloszenia[k].Kurs; '</p>'
-});
+	});
 }
 
 
@@ -90,8 +91,10 @@ function dataPrintFb(){
     var createdTime = day+"."+month+"."+year+"   godz. "+time;
     let box = document.getElementById('sFb');
     //box.className = "smallMargin2";
-    box.innerHTML = box.innerHTML + '<p class="data">' + createdTime + '<p>'
+    if(posts.data[k].message){
+	box.innerHTML = box.innerHTML + '<p class="data">' + createdTime + '<p>'
     box.innerHTML = box.innerHTML + '<p class="tresc">' + posts.data[k].message + '</p>'
+	}
     console.log(createdTime);
 
 });
